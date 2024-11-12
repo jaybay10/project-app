1d6a73a980da8b0e7f962be95398af8342437612
jasonBayford <jason.bayford@investec.com>
2024-11-12 13:56:06 +0200

    creating an interface

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../Shared/Interface/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
   imports: [],
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.scss'
+  styleUrl: './appointment-list.component.scss',
 })
 export class AppointmentListComponent {
-
+  appointment: Appointment = {
+    id: 1,
+    title: 'take dog for a walk',
+    date: new Date('2023-07-30'),
+  };
 }
