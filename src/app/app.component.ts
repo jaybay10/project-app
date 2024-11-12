7e1ac5177da7ae5c82f883b0a24216235f95152e
jasonBayford <jason.bayford@investec.com>
2024-11-12 13:19:05 +0200

    project initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AppointmentListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'project-app';
