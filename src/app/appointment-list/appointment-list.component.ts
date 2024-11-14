ddc0ab8cb17bbfea95d315a9df5e6d53cc4c42c3
jasonBayford <jason.bayford@investec.com>
2024-11-14 09:38:46 +0200

    Implementing forms validation and databinding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../Shared/Interface/appointment';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.scss',
 })
-export class AppointmentListComponent {
-  appointment: Appointment = {
-    id: 1,
-    title: 'take dog for a walk',
-    date: new Date('2023-07-30'),
-  };
+export class AppointmentListComponent implements OnInit {
+  form!: FormGroup;
+  appointments: Appointment[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  buildForm(): void {
+    this.form = this.fb.group({
+      id: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.appointments.push(this.form.value);
+      console.log(this.appointments);
+      this.form.reset();
+    } else {
+      alert('Please fill out the form before submitting');
+    }
+  }
 }
